a2c02ae9b6d7d05c4993677c57d81dfeef1eb690
Darryn Codes <hello@darryncodes.co.uk>
2022-04-28 19:13:51 +0100

    reconciling version control

--- src/components/Layout/Card.js
+++ src/components/Layout/Card.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 
 import Content from './Content';
@@ -28,30 +27,6 @@ const Card = () => {
   return (
     <main className={styles.card}>
       <Content onAddRating={setRatingData} />
-=======
-import React from 'react';
-
-import Content from './Content';
-// import Result from './Result';
-import styles from './Card.module.css';
-
-const Card = () => {
-  const addRatingHandler = rating => {
-    console.log('In Card.js');
-    console.log(rating);
-    return rating;
-  };
-
-  //   if (addRatingHandler.isSubmitted === true) {
-  //     <main className={styles.card}>
-  //       <Result />;
-  //     </main>;
-  //   }
-
-  return (
-    <main className={styles.card}>
-      <Content onAddRating={addRatingHandler} />
->>>>>>> 90eb7576f7521085899b425e815c1427d5f87132
     </main>
   );
 };
